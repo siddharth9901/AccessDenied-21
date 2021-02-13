521eb88d0a9c884b2788e9e5bcc850486d4869a7
siddharth9901 <58816993+siddharth9901@users.noreply.github.com>
2021-02-13 13:35:06 +0530

    Added Prizes View
    I have added prizes view and added it to info.

--- accessDenied/infoViewController.swift
+++ accessDenied/infoViewController.swift
@@ -28,7 +28,8 @@ class infoViewController: UIViewController {
                                    infoTopics(title: "About Access Denied", details: "Access Denied is a 36-hour hackathon organized by IETE-ISF for innovators across the country. It is an great opportunity to put your technical skills to use. Apply your textbook knowledge to real life ideas and stand a chance to win exclusive goodies, schwags, certificates and cash prizes upto Rs. 90,000. Mark your calendars for 19th to 21st March 2021.", image: UIImage(named: "Hackathon Logo")!, icon: "AD_white"),
                                    infoTopics(title: "About IETE", details: "The Institution of Electronics and Telecommunication Engineers (IETE) is India’s leading recognized professional society devoted to the advancement of Science and Technology of Electronics, Telecommunication and IT. Government of India has recognized IETE as a Scientific and Industrial Research Organization (SIRO) and also notified as an educational Institution of national eminence. The objectives of IETE focus on advancing electro-technology. The IETE conducts and partners technical meetings, conferences, symposia, and exhibitions all over India, publishes technical journals and providescontinuing education as well as career advancement opportunities to its members.", image: UIImage(named: "IETE WHITE")!, icon: "IETE WHITE"),
                                    infoTopics(title: "Developer Info", details: " ", image: UIImage(named: "Hackathon Logo")!, icon: "DEV_White"),
-                                   infoTopics(title: "Contact Us", details: " ", icon: "contactUs_white")]
+                                   infoTopics(title: "Contact Us", details: " ", icon: "contactUs_white"),
+                                    infoTopics(title: "Prizes", details: "",  icon: "prize_white")]
 
     
     //var sponsors
@@ -89,6 +90,10 @@ extension infoViewController: UITableViewDelegate, UITableViewDataSource {
         }
         else if infoCells[indexPath.row].title == "Tracks"{
             self.performSegue(withIdentifier: "tracks", sender: self)
+        }
+        
+        else if infoCells[indexPath.row].title == "Prizes"{
+            self.performSegue(withIdentifier: "prizes", sender: self)
         }
         
         else if infoCells[indexPath.row].title == "Developer Info"{
